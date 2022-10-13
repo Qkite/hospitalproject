3a7a4ed6f67a6326dc9c6073c544a855478dda6f
Qkite <yeonjigu4@gmail.com>
2022-10-13 14:11:39 +0900

    hospital parser 프로젝트 - 진료과목 체크

--- src/main/java/com/seoulhospital/domain/Hospital.java
+++ src/main/java/com/seoulhospital/domain/Hospital.java
@@ -11,13 +11,17 @@ public class Hospital {
     private int emergencyRoom;
 
 
-    public Hospital(String id, String address, String category,String name, int emergencyRoom){
+
+    private String addtionalInfo;
+
+
+    public Hospital(String id, String address, String category,String name, int emergencyRoom, String additionalInfo){
         this.id = id;
         this.address = address;
         this.name = name;
         this.emergencyRoom = emergencyRoom;
         this.category = category;
-
+        this.addtionalInfo = additionalInfo;
 
     }
 
@@ -44,11 +48,25 @@ public class Hospital {
     }
 
     public String getSubdivision() {
-        return subdivision;
+        String infos = this.name + this.addtionalInfo;
+        String[] inputCheck = {"내과", "외과", "소아", "피부", "성형", "정형외과", "척추", "교정", "산부인과", "관절", "봉합", "화상", "골절", "영유아", "안과", "가정의학과", "비뇨기과", "치과"};
+        String result = "";
+
+        for (String department:inputCheck) {
+            if(infos.contains(department)){
+                result += department;
+            }
+        }
+
+        return result;
     }
 
     public int getEmergencyRoom() {
         return emergencyRoom;
+    }
+
+    public String getAddtionalInfo() {
+        return addtionalInfo;
     }
 
 }
